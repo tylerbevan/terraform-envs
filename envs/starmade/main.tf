94370ecc103a7fd38275a4cf83a1226968b4c54b
Tyler Bevan <tbevan12@gmail.com>
2020-04-30 20:02:01 -0600

    Updates

--- envs/starmade/main.tf
+++ envs/starmade/main.tf
@@ -1,19 +1,19 @@
-resource "digitalocean_droplet" "kf2" {
+resource "digitalocean_droplet" "starmade" {
   image  = "centos-8-x64"
-  name   = "killingfloor2"
+  name   = "starmade"
   region = "sfo2"
   size   = "s-2vcpu-2gb"
   ssh_keys = [data.digitalocean_ssh_key.ssh_key.id]
 }
 
-resource "cloudflare_record" "kf2" {
+resource "cloudflare_record" "starmade" {
   zone_id = lookup(data.cloudflare_zones.filtered.zones[0], "id")
-  name    = "kf2"
-  value   = digitalocean_droplet.kf2.ipv4_address
+  name    = "starmade"
+  value   = digitalocean_droplet.starmade.ipv4_address
   type    = "A"
   ttl     = 600
 }
 
 output "ip_addr" {
-  value   = digitalocean_droplet.kf2.ipv4_address
+  value = digitalocean_droplet.starmade.ipv4_address
 }
